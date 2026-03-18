27e6c3f9dd57584591002bb85fcdc5e93f525d72
Buzzvitalize <buzzvitalize@gmail.com>
2026-03-18 11:37:03 -0400

    Delete Eterna Guerra Online directory

--- Eterna Guerra Online/Localization/en_us/UI/XML/Battle.lua
+++
@@ -1,4 +0,0 @@
-﻿--czq--
-function SetIconVisible()
-	GameAPI:SetIconVisible();
-end
--- Eterna Guerra Online/Localization/en_us/UI/XML/NpcFun/NpcFunFallback.lua
+++
@@ -1,6 +0,0 @@
-UIList_Fallback = {
-					{1,101,TEXT,NF_L0_1005}
-}
-
-local win = UIAPI:GetElement("FirstWin")
-UIMAKER:Create(win,'Fallback')
